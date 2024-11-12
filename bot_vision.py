a3b701b8098bc9efdd18c615557369f006cb4739
UsumiMin <128300344+UsumiMin@users.noreply.github.com>
2024-11-12 23:24:32 +0500

    Updated code

--- bot_vision.py
+++ bot_vision.py
@@ -2,36 +2,30 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
-from aiogram.types import (
-    ReplyKeyboardRemove,
-    ReplyKeyboardMarkup,
-    KeyboardButton,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-)
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# from config_reader import config
-
-# Для записей с типом Secret* необходимо
-# вызывать метод get_secret_value(),
-# чтобы получить настоящее содержимое вместо '*****'
-# Объект бота
-# bot = Bot(token=config.bot_token.get_secret_value())
-bot = Bot(token="7544782847:AAG2cOJdgRprwf2vo2OPCDVkrYbdLYDBqSA")
+bot = Bot(token="7544782847:AAGbpPxNuyvUT5TAyfMOge0SFb5G9QD2tIw")
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
-
 # Диспетчер
 dp = Dispatcher()
+categories = {
+    "Верх": 1,
+    "Низ": 2,
+    "Обувь": 3,
+    "Комплекты": 4,
+    "Акссесуары": 5,
+    "Парфюмерия": 6,
+}
+categ = None
+
+
+def one_of_categories(clothes):
+    return categories[clothes]
 
 
 # Хэндлер на команду /start
-"""@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!")"""
-
-
 @dp.message(Command("start"))
 async def send_welcome(message: types.Message):
     kb = [
@@ -57,15 +51,11 @@ async def start_work(message: types.Message):
         [types.KeyboardButton(text="Верх"), types.KeyboardButton(text="Низ")],
         [
             types.KeyboardButton(text="Обувь"),
-            types.KeyboardButton(text="Нательное бельё"),
+            types.KeyboardButton(text="Комплекты"),
         ],
         [
-            types.KeyboardButton(text="Бижутерия"),
             types.KeyboardButton(text="Аксессуары"),
-        ],
-        [
-            types.KeyboardButton(text="Верхняя одежда"),
-            types.KeyboardButton(text="Комбинированная"),
+            types.KeyboardButton(text="Парфюмерия"),
         ],
         [types.KeyboardButton(text="Назад")],
     ]
@@ -84,35 +74,38 @@ async def url_command(message: types.Message):
     buttons = [
         [
             InlineKeyboardButton(
-                text="Главный программист(почему?(всё хорошо(милфа)))",
+                text="Главный программист (Кривелева Катерина)",
                 url="https://t.me/usumimin",
             )
         ],
         [
             InlineKeyboardButton(
-                text="Аналитик данных(группа поддержки(профессиональный мемолог))",
+                text="Аналитик данных (Селихова Таисия)",
                 url="https://t.me/Gerpook",
             )
         ],
         [
             InlineKeyboardButton(
-                text="Дизайнер(Физик-ядерщик(доминатрикс))",
+                text="Аналитик данных (Лобова Кристина)",
                 url="https://t.me/Niftylmao",
             )
         ],
         [
             InlineKeyboardButton(
-                text="Тимлидер(pussy girl(pick me))", url="https://t.me/asmodeykaa"
+                text="Тимлидер (Кириченко Дана)", url="https://t.me/asmodeykaa"
             )
         ],
     ]
+
     urlkb = InlineKeyboardMarkup(inline_keyboard=buttons)
     await message.answer("Контакты разработчиков:", reply_markup=urlkb)
 
 
 @dp.message(F.text.lower() == "инструкция")
 async def instruction(message: types.Message):
-    await message.answer("Категория - фото - счастье")
+    await message.answer(
+        "Выбираете категорию, загружаете нужное фото, бот подбирает вам ссылки с маркетплейсов с вашим товаром."
+    )
 
 
 @dp.message(Command("stop"))
@@ -123,24 +116,36 @@ async def stop_bot(message: types.Message):
     await dp.stop_polling()
 
 
-"""@dp.message(Command("reply"))
-async def cmd_reply(message: types.Message):
-    await message.reply('Это ответ с "ответом"')"""
-
-
 @dp.message(F.text.lower() == "верх")
 async def with_puree(message: types.Message):
-    await message.reply("Отличный выбор!", reply_markup=types.ReplyKeyboardRemove())
+    await message.answer("Отправьте фото, на котором вы хотите найти одежду")
 
 
-@dp.message(F.text.lower() == "а это?")
-async def without_puree(message: types.Message):
-    await message.reply("Так невкусно!")
-
-
-@dp.message()
+"""@dp.message(categ)
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    await message.answer("Отправьте фото, на котором вы хотите найти одежду")"""
+
+global file_photo_id
+# file_photo_id = ""
+bot: Bot
+global fp
+
+
+@dp.message(F.photo)
+async def send_photo_copy(message: types.Message):
+    await message.answer(
+        "Пока я без ии мало что могу( Но я отправлю ваше фото вам обратно."
+    )
+    await message.send_copy(chat_id=message.chat.id)
+
+
+@dp.message(F.text)
+async def send_photo(message: types.Message):
+    await message.answer("Я не понимаю что вы хотите")
+    """bot.send_photo(
+        chat_id=message.from_user.id,
+        photo=fp,
+    )"""
 
 
 # Запуск процесса поллинга новых апдейтов
@@ -150,28 +155,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-"""
-Для более динамической генерации кнопок можно воспользоваться сборщиком клавиатур. Нам пригодятся следующие методы:
-
-add(<KeyboardButton>) — добавляет кнопку в память сборщика;
-adjust(int1, int2, int3...) — делает строки по int1, int2, int3... кнопок;
-as_markup() — возвращает готовый объект клавиатуры;
-button(<params>) — добавляет кнопку с заданными параметрами, тип кнопки (Reply или Inline) определяется автоматически.
-Создадим пронумерованную клавиатуру размером 4×4:
-
-# новый импорт!
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-
-@dp.message(Command("reply_builder"))
-async def reply_builder(message: types.Message):
-    builder = ReplyKeyboardBuilder()
-    for i in range(1, 17):
-        builder.add(types.KeyboardButton(text=str(i)))
-    builder.adjust(4)
-    await message.answer(
-        "Выберите число:",
-        reply_markup=builder.as_markup(resize_keyboard=True),
-    )
-"""
